ef7a3efc0a40641e35a987315d02f82a3bafff07
Michal Wiglasz <michal.wiglasz@gmail.com>
2014-04-13 13:44:52 +0200

    Add Pavel Dedik to authors (who originally added gevent support)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='sklikapi',
     version='1.0.0',
-    author='Eduard Veleba, Michal Wiglasz',
+    author='Eduard Veleba, Michal Wiglasz, Pavel Dedik',
     description=('Python library for easier access to the Sklik.cz API,'
                  'a pay-per-click advertising system, operated by Seznam.cz.'),
     packages=find_packages(exclude=['tests']),
