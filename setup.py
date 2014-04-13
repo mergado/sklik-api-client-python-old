16d4149de118cb0334450c1386af1a09b27b0449
Michal Wiglasz <michal.wiglasz@gmail.com>
2014-04-13 13:30:37 +0200

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sklikapi',
+    version='1.0.0',
+    author='Eduard Veleba, Michal Wiglasz',
+    description=('Python library for easier access to the Sklik.cz API,'
+                 'a pay-per-click advertising system, operated by Seznam.cz.'),
+    packages=find_packages(exclude=['tests']),
+    install_requires=[],
+    tests_require=[],
+    include_package_data=True,
+)
