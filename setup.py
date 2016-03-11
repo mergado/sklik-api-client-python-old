a124e6fd6ababc5bd33fb56571eb6bb6095a391e
Pavel Dedík <dedikx@gmail.com>
2016-03-11 11:17:15 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='sklikapi',
-    version='2.0.2',
-    author='Eduard Veleba, Michal Wiglasz, Pavel Dedik',
+    version='2.0.4',
+    author='Eduard Veleba, Michal Wiglasz, Pavel Dedik, Lukas Sevcik',
     description=('Python library for easier access to the Sklik.cz API,'
                  'a pay-per-click advertising system, operated by Seznam.cz.'),
     packages=find_packages(exclude=['tests']),
